8b18c9447a09807b21fedf0fd4704dc2506074c7
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-07-06 12:49:21 -0400

    Added scaffolds for categories and entries

---
+++ db/migrate/20190706164911_create_entries.rb
@@ -0,0 +1,13 @@
+class CreateEntries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :entries do |t|
+      t.string :title
+      t.text :body
+      t.boolean :secret
+      t.references :category, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
